31b6083549f19358d34afd07a08f6d26cda62334
Stephen Huysman <shuysman@gmail.com>
2024-02-15 10:22:28 -0700

    only calculate mean for 2020 because long run time

--- assignment4/assignment4-parallel.R
+++ assignment4/assignment4-parallel.R
@@ -1,5 +1,5 @@
 ### CSCI 594 Assignment 4
-### Calculate average 2010-2020 historical annual precip for the Greater Yellowstone Ecosystem
+### Calculate average 2020 historical annual precip for the Greater Yellowstone Ecosystem
 ### Run in parallel and sequentially, compare run times
 
 time <- Sys.time()
@@ -19,19 +19,10 @@ rain_file_pattern <- "rain_.*_subset.nc"
 rain_files <- list.files(wb_data_dir, pattern = rain_file_pattern, full.names = TRUE)
 
 rain <- rast(rain_files) %>%
-    subset(year(time(.)) >= 2010) %>% subset(year(time(.)) <= 2020) %>% ### filter to years of interest
+    subset(year(time(.)) == 2020) %>% ### filter to years of interest
     clamp(lower = 0) ### Clamp off weird negative pixels over bodies of water, etc.
 
-### Calculate mean accumulate SWE across history 
-
-### Mean rain for each year
-annual_mean_rain <- terra::tapp(rain,
-                         fun = \(i) mean(i), ### Use base R mean() to allow parallelization
-                         index = "years",
-                         cores = cores)
-
-### Mean rain for all years
-mean_rain <- terra::app(annual_mean_rain,
+mean_rain <- terra::app(rain,
                         fun = \(i) mean(i),  ### Use base R mean() to allow parallelization
                         cores = cores)
 
